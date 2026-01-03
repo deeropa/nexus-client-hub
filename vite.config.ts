a3f66ab8a659fe4347541d04297b814f674fb1ef
deeropa <idiotasdomas@gmail.com>
2026-01-03 00:35:46 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc"
 import path from "path"
 
 export default defineConfig({
-  base: "/nexus-client-hub/",
   plugins: [react()],
   resolve: {
     alias: {
